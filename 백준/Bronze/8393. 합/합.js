c4ea3bcf8c46ee4283c3fe9311dea522d51546a9
mongzii <120315387+mongzii@users.noreply.github.com>
2023-07-29 22:43:11 +0900

    [Bronze V] Title: 합, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -2,9 +2,6 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let n = Number(input[0]);
-let summary = 0;
 
-for(let i=1; i <= n; i++){
-    summary = summary + i;
-}
-console.log(summary);
+//등차수열의 합 공식
+console.log(n * (n+1) / 2);
