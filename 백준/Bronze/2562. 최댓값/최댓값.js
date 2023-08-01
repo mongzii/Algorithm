35e1981469d719fd712f3a3b4801d8bc9f4da02e
mongzii <120315387+mongzii@users.noreply.github.com>
2023-08-01 12:43:14 +0900

    [Bronze III] Title: 최댓값, Time: 152 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let maxValue = 0;
+let maxIndex = 0;
+
+for(let i=0; i<9; i++){
+    let data = Number(input[i]);
+    if(maxValue < data){
+        maxValue = data;
+        maxIndex = i;
+    }
+}
+console.log(maxValue);
+console.log(maxIndex + 1);
