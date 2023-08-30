da8f297d7a71fe6c86e413259bac1705f2001b22
mongzii <120315387+mongzii@users.noreply.github.com>
2023-08-30 14:00:50 +0900

    [unrated] Title: flag에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,10 @@
+function solution(a, b, flag) {
+    if(flag === true){
+        return a+b;
+    }
+    else if(flag === false){
+        return a-b;
+    }
+}
+// flag === true이면 a+b
+//flag === false이면  a-b
