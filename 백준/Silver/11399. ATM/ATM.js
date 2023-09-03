8cf93f442a35662fb8170b2c375365d5c5d01962
mongzii <120315387+mongzii@users.noreply.github.com>
2023-09-03 14:13:54 +0900

    [Silver IV] Title: ATM, Time: 156 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+
+arr.sort((a,b) => a-b);
+
+let answer = 0;
+let summary = 0;
+for(let i=0; i<n; i++){
+    summary += arr[i]  //i번째 사람이 기다린 총시간
+    answer += summary;
+}
+console.log(answer);
