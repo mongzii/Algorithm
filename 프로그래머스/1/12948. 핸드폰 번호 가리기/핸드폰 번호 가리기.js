8a52812cd768a45808a96fcead525484d7424eda
mongzii <120315387+mongzii@users.noreply.github.com>
2023-11-02 13:46:48 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,16 @@
+function solution(phone_number) {
+    // var answer = '';
+    // return answer;
+    //map이용하면 좋을듯
+    //길이가 11이면 11-4=7   (0,7)
+  let front =  phone_number.slice(0,phone_number.length-4);
+    let back = phone_number.slice(phone_number.length-4, phone_number.length);
+    let aaa = back.split('')
+    
+    let bbb =(front.split('')).map((el)=>  "*")
+    
+   return (String(bbb.concat(aaa).join('')))
+   // let dfdf =(front.split('')).map((el)=>  "*").join('');
+    
+    
+}
